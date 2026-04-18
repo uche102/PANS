c9a6448603100b5ad154ee68a0657dbf188365eb
uche102 <nwakoruche192@gmail.com>
2026-04-18 04:10:45 +0100

    fixed .env issues

--- pans-backend/server.js
+++ pans-backend/server.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 import { Resend } from "resend";
+import dotenv from "dotenv";  
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
